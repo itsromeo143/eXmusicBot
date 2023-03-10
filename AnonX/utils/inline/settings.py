72bbf6a1b12b5230d66a45f58be859315eeeed33
Mr Romeo <126165195+itsromeo143@users.noreply.github.com>
2023-03-11 02:23:48 +0530

    Add files via upload

--- AnonX/utils/inline/settings.py
+++ AnonX/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="• sᴜᴩᴩᴏʀᴛ •", url=f"{SUPPORT_GROUP}",
+                text="• sᴜᴩᴩᴏʀᴛ •", url=f"{hindichat_house}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
