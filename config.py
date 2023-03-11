00dca5388aa46d4c6162515836e61da9e58ae0fd
Mr Romeo <126165195+itsromeo143@users.noreply.github.com>
2023-03-11 07:13:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://www.linkpicture.com/q/IMG_20230311_071003_234.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
